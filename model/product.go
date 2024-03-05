39e77ac9f136b62c05eec636655117489f9817f3
skay <skai-zhang@hotmail.com>
2024-03-05 15:32:10 +0800

    update product model

--- model/product.go
+++ model/product.go
@@ -1,1 +1,21 @@
 package model
+
+import "xorm.io/xorm"
+
+type Product struct {
+	Id int64 `json:"id"`
+	// 制品处理器(1Maven、2NPM)
+	Processor int64 `json:"processor"`
+	// 制品组
+	Group string `json:"group"`
+	// 制品名
+	Name string `json:"name"`
+	// 添加时间
+	AddTime string `json:"addTime"`
+	// 更新时间
+	UpdateTime string `json:"updateTime"`
+}
+
+type ProductModel struct {
+	DB *xorm.Engine
+}
